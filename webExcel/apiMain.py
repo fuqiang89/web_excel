95e784e6b558e84f1726a77da01f6fbb053f1e5b
fuqiang89 <wfuqiang89@gmail.com>
2014-12-03 00:42:49 +0800

    add  api inteface

---
+++ webExcel/apiMain.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+__author__ = 'fuqiang'
+import os,sys
+from tornado import template
+from tornado.web import RequestHandler
+import tornado.web
+from Storage import storage
+from utils import *
+from tornado.escape import json_encode
+from basehandler import basehandler
+from config import TP
+from moudle.srv_m import table_operate
+from moudle.table_orm import table_orm
+from moudle.operate_register import operate_register
+from config import dataPath
+tl=template.Loader(os.path.join(TP, "webExcel/srv_html"))
+table_operate=table_operate()
+table_orm=table_orm()
+operate_register=operate_register()
+class  API(basehandler):
+    @tornado.web.authenticated
+    def get(self, *args, **kwargs):
+        i=self.input()
+        i.username=self.current_user
+        i.fields=table_orm.get_fields("srv_table")
+        self.render("xProfile.html",i=i)
